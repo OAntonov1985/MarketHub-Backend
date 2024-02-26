d844b03e0d8fd0b5c2a219dc0a9a6d671d1031b7
OAntonov1985 <aantonov1985@gmail.com>
2024-02-26 15:13:44 +0200

    26.02

--- index.js
+++ index.js
@@ -19,6 +19,25 @@ connectToDb((err) => {
     }
 });
 
+
+app.get('/categories', (req, res) => {
+    const goods = [];
+    db
+        .collection('categories')
+        .find()
+        .forEach((item) => goods.push(item))
+        .then(() => {
+            res
+                .status(200)
+                .json(goods);
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ error: "Упс... Щось пішло не так..." })
+        })
+});
+
 app.get('/goods/top-sellers', (req, res) => {
     const goods = [];
     db
@@ -37,132 +56,42 @@ app.get('/goods/top-sellers', (req, res) => {
         })
 });
 
-// app.get("/api/v1/books", async (req, res) => {
-//     const { limit = 5, orderBy = "name", sortBy = "asc", keyword } = req.query;
-//     let page = +req.query?.page;
+app.get('/sales', (req, res) => {
+    const goods = [];
+    db
+        .collection('sales')
+        .find()
+        .forEach((item) => goods.push(item))
+        .then(() => {
+            res
+                .status(200)
+                .json(goods);
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ error: "Упс... Щось пішло не так..." })
+        })
+});
 
-//     if (!page || page <= 0) page = 1;
+app.get('/categorie/:categoryId', (req, res) => {
+    const dataArray = [];
+    const categoryId = req.params.categoryId;
 
-//     const skip = (page - 1) * + limit;
+    db
+        .collection('subcategories')
+        .find({ parent_category_id: categoryId })
+        .forEach((item) => dataArray.push(item))
+        .then(() => {
+            res
+                .status(200)
+                .json(dataArray);
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ error: "Упс... Щось пішло не так..." })
+        })
+});
 
-//     const query = {};
-
-//     if (keyword) query.name = { $regex: keyword, $options: "i" };
-
-//     const key = `Book::${JSON.stringify({ query, page, limit, orderBy, sortBy })}`
-//     let response = null
-//     try {
-//         const cache = await redis.get(key)
-//         if (cache) {
-//             response = JSON.parse(cache)
-//         } else {
-//             const data = await BookModel.find(query)
-//                 .skip(skip)
-//                 .limit(limit)
-//                 .sort({ [orderBy]: sortBy });
-//             const totalItems = await BookModel.countDocuments(query);
-
-//             response = {
-//                 msg: "Ok",
-//                 data,
-//                 totalItems,
-//                 totalPages: Math.ceil(totalItems / limit),
-//                 limit: +limit,
-//                 currentPage: page,
-//             }
-
-//             redis.setex(key, 600, JSON.stringify(response))
-//         }
-
-//         return res.status(200).json(response);
-//     } catch (error) {
-//         return res.status(500).json({
-//             msg: error.message,
-//         });
-//     }
-// });
-
-// app.get("/api/v1/books/:id", async (req, res) => {
-//     try {
-//         const data = await BookModel.findById(req.params.id);
-
-//         if (data) {
-//             return res.status(200).json({
-//                 msg: "Ok",
-//                 data,
-//             });
-//         }
-
-//         return res.status(404).json({
-//             msg: "Not Found",
-//         });
-//     } catch (error) {
-//         return res.status(500).json({
-//             msg: error.message,
-//         });
-//     }
-// });
-
-// app.post("/api/v1/books", async (req, res) => {
-//     try {
-//         const { name, author, price, description } = req.body;
-//         const book = new BookModel({
-//             name,
-//             author,
-//             price,
-//             description,
-//         });
-//         const data = await book.save();
-//         deleteKeys('Book')
-//         return res.status(200).json({
-//             msg: "Ok",
-//             data,
-//         });
-//     } catch (error) {
-//         return res.status(500).json({
-//             msg: error.message,
-//         });
-//     }
-// });
-
-// app.put("/api/v1/books/:id", async (req, res) => {
-//     try {
-//         const { name, author, price, description } = req.body;
-//         const { id } = req.params;
-
-//         const data = await BookModel.findByIdAndUpdate(
-//             id,
-//             {
-//                 name,
-//                 author,
-//                 price,
-//                 description,
-//             },
-//             { new: true }
-//         );
-//         deleteKeys('Book')
-//         return res.status(200).json({
-//             msg: "Ok",
-//             data,
-//         });
-//     } catch (error) {
-//         return res.status(500).json({
-//             msg: error.message,
-//         });
-//     }
-// });
-
-// app.delete("/api/v1/books/:id", async (req, res) => {
-//     try {
-//         await BookModel.findByIdAndDelete(req.params.id);
-//         deleteKeys('Book')
-//         return res.status(200).json({
-//             msg: "Ok",
-//         });
-//     } catch (error) {
-//         return res.status(500).json({
-//             msg: error.message,
-//         });
-//     }
-// });
 
