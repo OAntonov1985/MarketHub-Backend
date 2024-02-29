f1cfac4b1aa01b89b523674110c50aab0ce2f8ec
OAntonov1985 <aantonov1985@gmail.com>
2024-02-29 10:50:53 +0200

    29.02

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ app.get('/sales', (req, res) => {
 app.get('/categorie/:categoryId', (req, res) => {
     const dataArray = [];
     const categoryId = req.params.categoryId;
+    console.log(categoryId)
 
     db
         .collection('subcategories')
@@ -98,6 +99,7 @@ app.get('/goods/:goodId', (req, res) => {
     const goodId = req.params.goodId;
     console.log(goodId)
 
+
     db
         .collection('goods')
         .findOne({ id: goodId })
@@ -107,6 +109,50 @@ app.get('/goods/:goodId', (req, res) => {
             } else {
                 res.status(404).json({ error: "Товару не знайдено" });
             }
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ error: "Упс... Щось пішло не так..." })
+        })
+});
+
+app.get('/goods/categories/:categoryId', (req, res) => {
+    const dataArray = [];
+    const categoryId = req.params.categoryId;
+
+
+    db
+        .collection('ComputerEngineering')
+        .find({ "category_details.id": categoryId })
+        .forEach((item) => dataArray.push(item))
+        .then(() => {
+            res
+                .status(200)
+                .json(dataArray);
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ error: "Упс... Щось пішло не так..." })
+        })
+});
+
+
+app.get('/goods/subcategories/:subCategoryId', (req, res) => {
+    const dataArray = [];
+    const subCategoryId = req.params.subCategoryId;
+    console.log(req.params)
+
+
+    db
+        .collection('ComputerEngineering')
+        .find({ "sub_category_detail.id": subCategoryId })
+        .forEach((item) => dataArray.push(item))
+        .then(() => {
+            res
+                .status(200)
+                .json(dataArray);
         })
         .catch(() => {
             res
